5012fcb73dc7f3e87862aa58dc7e36201c01d29b
anusha-garre <41730901+anusha-garre@users.noreply.github.com>
2018-07-27 10:52:57 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number;
+		Scanner sc=new Scanner(System.in);
+		
+		number=sc.nextInt();
+	           if(number%2==0)
+                	   { 
+	      System.out.println("even number");
+	    }
+	    else 
+	    {
+	       System.out.println("odd number");	
+	     }
+	    // your code goes here
+	}
+}// your code goes here
+	
